5ad3fa6877b90d4654118ecd1380b52bdaa537f4
Alex Hedley <alex.hedley@me.com>
2023-11-02 19:11:09 +0000

    Added wanted pops

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,30 @@
 var myApp = angular.module('myApp',[]);
 myApp.controller('myController', function ($scope, $http, $q, $filter) {
 
-    $scope.events = [];
+    $scope.pops = [];
+    $scope.wantedpops = [];
 
     $scope.init = function () {
         getData();
+        getWantedData();
     }
 
     getData = () =>  {
         var file = 'data/pops.json';
 
         $http.get(file)
-        .then(function(response) {
-            $scope.pops = response.data.pops;
-        });
+            .then(function(response) {
+                $scope.pops = response.data.pops;
+            });
+    };
+
+    getWantedData = () =>  {
+        var file = 'data/wanted.json';
+
+        $http.get(file)
+            .then(function(response) {
+                $scope.wantedpops = response.data.pops;
+            });
     };
 
     $scope.init();
