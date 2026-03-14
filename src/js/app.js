c823117aff740fadc3f107cdee12d8514d560bb2
Alex Hedley <1573469+AlexHedley@users.noreply.github.com>
2026-03-14 17:44:04 +0000

    extract filters and add label to sort

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,5 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.filter("groupBy", ["$parse", function($parse) {
-    return function(list, groupBy) {
-        var grouped = {};
-        var parse = $parse(groupBy);
-        angular.forEach(list, function(item) {
-            var group = parse(item);
-            if (!grouped[group]) {
-                grouped[group] = [];
-            }
-            grouped[group].push(item);
-        });
-        return grouped;
-    };
-}]);
-
 myApp.controller('myController', function ($scope, $http, $q, $filter) {
 
     $scope.pops = [];
@@ -45,13 +30,12 @@ myApp.controller('myController', function ($scope, $http, $q, $filter) {
 
     $scope.orderOptions = [
         { name:'name' },
-        { name:'updated_at' },
-        { name:'created_at' }
+        { name:'number' }
     ];
 
     $scope.orderProp = 'name';
     $scope.setOrder = function (orderProp) {
-        console.log(orderProp);
+        // console.log(orderProp);
         $scope.orderProp = orderProp;
     };
 
